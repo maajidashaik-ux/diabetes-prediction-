d68a693543a8c6bd9ce09fe1ab6ccd258eb37e37
Maajida <maajidashaik@gmail.com>
2026-05-02 00:13:41 +0530

    Refactor diabetes.py and add score output
    Removed unnecessary imports and added print statement for cross-validation scores.

--- diabetes.py
+++ diabetes.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import recall_score,precision_score,f1_score
 
 data = pd.read_csv(r"C:\Users\maaji\Downloads\diabetes.csv")
 
